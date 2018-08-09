ecfcd48c79f15b31f41c38968635c688afb0e2e3
gofar09 <gordon.d.farrell@gmail.com>
2018-08-09 10:08:00 -0400

    Bugfix -Remove extra slash on heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://ancient-citadel-83605.herokuapp.com/',
+  production: 'https://ancient-citadel-83605.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
